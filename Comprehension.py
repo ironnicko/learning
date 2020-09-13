75574ecc538be1ccb06f256e6a2c25817b5c4864
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2020-09-14 01:39:59 +0530

    Update Comprehension.py

--- Comprehension.py
+++ Comprehension.py
@@ -6,3 +6,5 @@ print(*[i for i in [f'{i[0]}:{i[1]}\n' for i in list(zip(a,b))]])
 g = {i:i**100 for i in range(1,21)}
 print(*[i for i in [f'{i}:{g[i]}\n' for i in g]])
 print(f'{22/7:.100f}')
+for i,names in enummerate(a):
+  print(f'{i+1}:{name}')#index
