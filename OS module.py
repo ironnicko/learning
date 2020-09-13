11c9f376639b8579685fda91cb8b1251ff04ea8a
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2020-09-14 01:48:15 +0530

    Update OS module.py

--- OS module.py
+++ OS module.py
@@ -1,3 +1,13 @@
 import os
 
-cwd = os.getcwd()
+cwd = os.getcwd()#current dir
+
+os.mkdir(f'{input('Name of Directory:')}')
+os.rmdir(f'{input('Name:')}')
+
+os.remove(f'{input('Name of file to remove:')}')
+
+os.chdir(f'{input('Change Directory:')}')
+os.listdir(f'{input('Path:')}')
+           
+os.system(f'{input('The terminal command')}')
