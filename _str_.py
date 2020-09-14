31ed943e2c9f315c58cf2203c7984333f382ed1e
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2020-09-14 11:33:13 +0530

    Update _str_.py

--- _str_.py
+++ _str_.py
@@ -7,7 +7,7 @@ class main():
         # self.name = input('Name:')
         # self.age = int(input('Age:'))
         self.week, self.month, self.date, self.time, self.year = datetime.now().strftime('%c').split()
-        self.time = self.time.join('n')
+        self.time = self.time.join(' ')
 
     def __str__(self):
         return f'{self.week} the {self.date}th, {self.year}'
