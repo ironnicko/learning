89688fc80ed19657f5c95660d5d8788ebf1fbf83
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2020-10-06 23:01:38 +0530

    Update Z_pattern.py

--- Z_pattern.py
+++ Z_pattern.py
@@ -1,5 +1,5 @@
 n = int(input('Enter the limit:'))
 print(*[i for i in range(1, n + 1)])
-for i in range(n, 1, -1):
+for i in range(n - 1, 1, -1):
     print(f' '*(2*(i-1)) + f'{i}')
 print(*[i for i in range(1, n+1)])
