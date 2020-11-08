7d105c1663a17b9717aff5e77eb2eb17b7a58a70
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2020-11-08 19:42:44 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// Wass up
+/*
+console.log("Hello");
+
+console.log("Kill you")
+
+let a;//let is for initializing
+
+console.log(a)
+
+
+a = 87;
+
+a = "Poopy pants"
+
+console.log(a);
+
+const gg = 885;
+
+console.log(gg);
+
+
+console.log("Gigi", "Hadid");
+
+
+let first, second, third, mix;
+
+first = 1;
+second = 2;
+third = 3;
+mix = [first, second, third];
+console.log(mix);*/
