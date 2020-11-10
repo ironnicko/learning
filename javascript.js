c5e57a492c3fefe794c6f7a944670ea0d85b3f1d
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2020-11-11 02:54:10 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -43,10 +43,64 @@ console.log(a.length)// a function to find the length of the item
 // string slicing
 
 const a = "Nikhil";
-console.log(a[3]);*/
+console.log(a[3]);
 
 // string functions
 
 console.log("GG man".toUpperCase());
 console.log("HEY".toLowerCase());
 console.log("man".endsWith("n"));
+const gg="Game Over";//can't be changed once declared
+
+//"var" a is global scope while "let" is for declaration inside the scope
+
+// Dictionaries are objects in JS
+var person = {
+	name: "Nikhil",
+	age: 17
+};
+let name1 = window.prompt("Enter your name");
+person.name = name1;
+console.log(person.name);
+// or you can use person["name"]
+
+var array = ["one", 2];
+array[0] = 1;
+console.log(array, array.length);
+
+//FUNCTIONS
+
+function greet(name, lastname){
+	console.log(name, lastname);
+}
+//greet("Nikhil", "Ivannan");
+
+function square(number){
+	return number**2;
+}
+console.log(square(2));
+
+//IF-ELSE loops which i figured out by myself
+
+let i=0;
+
+if (i<10){
+  console.log(i);
+  i++;
+}
+  else{
+    console.log(i**2);
+  }
+
+//WHILE loops again by moi
+
+while(i<10){
+  console.log(i);
+  i++;
+}
+
+//samething in FOR loop by moi
+
+for (;i<10;){
+  console.log(i);
+}*/
