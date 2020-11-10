491ab623d807ece6faf8dc86f2ac52d70eddf918
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2020-11-10 17:14:36 +0530

    Update 2instagram.py

--- 2instagram.py
+++ 2instagram.py
@@ -57,8 +57,8 @@ message_ = input("message >>>")
 
 class bot:
     def __init__(self, username, password, user, message):
-        self.username = "ferr6601"
-        self.password = "shemademecum"
+        self.username = input('enter username')
+        self.password = input('enter password')
         self.user = user
         self.message =  message
         self.base_url = 'https://www.instagram.com/'
@@ -87,9 +87,11 @@ class bot:
         time.sleep(2)
         self.bot.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div[2]/div/button').click()
         time.sleep(2)
-        self.bot.find_element_by_css_selector('textarea[placeholder="Message..."]').send_keys(self.message)
-        time.sleep(1)
-        self.bot.find_element_by_xpath('/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button').click()
+        while True
+            message = input('Enter your message')
+            self.bot.find_element_by_css_selector('textarea[placeholder="Message..."]').send_keys(message)
+            time.sleep(1)
+            self.bot.find_element_by_xpath('/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button').click()
 
 def init():     
     bot('bot_test0', 'abcdefg123456', usern, message_)
