e0d90c377fba845cbcc6c8b8fafc37fed051f25c
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2020-11-12 20:36:41 +0530

    Update text-string.py

--- text-string.py
+++ text-string.py
@@ -1,12 +1,29 @@
 import os
 from PIL import Image
-#os.system("pip install pytesseract")
 import pyautogui
 from pytesseract import *
 
-pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+array = []
+l = []
 
-img = Image.open("views.png")
-op = pytesseract.image_to_string(img, lang="fra")
+print("The available convertable images are: [if you dont find it here please copy-paste the image into the folder of the program]")
 
-print(op)
+for i in os.listdir():
+    if i.endswith("png"):
+        array.append(i)
+        
+for pos, i in enumerate(array):
+    print(pos, i)
+    l.append(pos)
+while True:
+    ch = int(input("Enter the required image file number shown above:"))
+    if ch in l:
+        pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        img = Image.open(f"{array[ch]}")
+        op = pytesseract.image_to_string(img)
+        print(op)
+        if input("Press 0 to exit") == "0":
+            break
+    else:
+        print("Wrong input... Try again")
+
