4a20ca0cd52e7df5c3fe81659a6b9f20a2245418
Nikhil Ivannan <nickonicko779@gmail.com>
2021-01-18 09:23:02 +0530

    z patterb

--- Z_pattern.py
+++ Z_pattern.py
@@ -1,5 +1,15 @@
-n = int(input('Enter the limit:'))
-print(*[i for i in range(1, n + 1)])
-for i in range(n - 1, 1, -1):
-    print(f' '*(2*(i-1)) + f'{i}')
-print(*[i for i in range(1, n+1)])
+# n = int(input('Enter the limit:'))
+# print(*[i for i in range(1, n + 1)])
+# for i in range(n - 1, 1, -1):
+#     print(f' '*(2*(i-1)) + f'{i}')
+# print(*[i for i in range(1, n+1)])
+
+result_str=""    
+for row in range(0,25):    
+    for column in range(0,25):     
+        if (((row == 0 or row == 24) and column >= 0 and column <= 24) or row+column==24):  
+            result_str=result_str+"*"    
+        else:      
+            result_str=result_str+" "    
+    result_str=result_str+"\n"    
+print(result_str)
